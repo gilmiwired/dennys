3d1d8c2fb92903c23da7203c85f560cc70529dc0
gilmi <65973449+gilmiwired@users.noreply.github.com>
2023-08-15 17:45:51 +0900

    手動登録tree,task追加
    キーが整数型でサポートされてないから無理やり文字列型で保存してる。
    tree = [
        [1, [2, 3, 4, 5, 6]],
        [2, [7, 8, 9]],
        [3, [10, 11, 12]],
        [4, [13, 14]],
        [5, [15, 16]],
        [6, [17, 18]]
    ]
    この形式(リストをネストした形式)で保存しようとしてたけどデータベース的には
    
    tree = {
            '1': [2, 3, 4, 5, 6],
            '2': [7, 8, 9],
            '3': [10, 11, 12],
            '4': [13, 14],
            '5': [15, 16],
            '6': [17, 18]
          };
    こっちの方(Map型+リスト)がいいみたい

--- dennys_web_app/lib/data_service.dart
+++ dennys_web_app/lib/data_service.dart
@@ -1,54 +1,62 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dennys_web_app/log/logger.dart';
 
 class DataService {
-    final FirebaseFirestore firestore = FirebaseFirestore.instance;
-
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
     Future<void> saveToFirestore() async {
-        final contentRef = firestore.collection('users').doc('user_ID').collection('content');
-        // Tree data
-        final tree = [
-            [1, [2, 3, 4, 5, 6]],
-            [2, [7, 8, 9]],
-            [3, [10, 11, 12]],
-            [4, [13, 14]],
-            [5, [15, 16]],
-            [6, [17, 18]]
-        ];
+      final contentRef = firestore.collection('users').doc('user_ID').collection('content');
+      final userRef = firestore.collection('users').doc('user_ID');
 
-        // Tasks data
-        final tasks = {
-            1: "ゲームを作る",
-            2: "デザイン",
-            3: "プログラム",
-            4: "グラフィックス",
-            5: "サウンド",
-            6: "テスト",
-            7: "コンセプト",
-            8: "キャラ・ストーリー",
-            9: "ルール・メカニクス",
-            10: "エンジン選択",
-            11: "キャラ動き",
-            12: "ロジック・AI",
-            13: "キャラ・背景アート",
-            14: "アニメーション",
-            15: "BGM",
-            16: "効果音",
-            17: "バグチェック",
-            18: "ユーザーテスト"
-        };
+      // Tree data
+      final tree = {
+        '1': [2, 3, 4, 5, 6],
+        '2': [7, 8, 9],
+        '3': [10, 11, 12],
+        '4': [13, 14],
+        '5': [15, 16],
+        '6': [17, 18]
+      };
 
-        for (var item in tree) {
-            final id = item[0];
-            final children = item[1];
-            await contentRef.doc('ID$id').set({
-                'title': tasks[id],
-                'children': children,
-                'status': "do",
-                'description': "説明"
-            });
-        }
+      // Tasks data
+      final tasks = {
+        '1': 'ゲームを作る',
+        '2': 'デザイン',
+        '3': 'プログラム',
+        '4': 'グラフィックス',
+        '5': 'サウンド',
+        '6': 'テスト',
+        '7': 'コンセプト',
+        '8': 'キャラ・ストーリー',
+        '9': 'ルール・メカニクス',
+        '10': 'エンジン選択',
+        '11': 'キャラ動き',
+        '12': 'ロジック・AI',
+        '13': 'キャラ・背景アート',
+        '14': 'アニメーション',
+        '15': 'BGM',
+        '16': '効果音',
+        '17': 'バグチェック',
+        '18': 'ユーザーテスト'
+      };
 
-        print('Data saved to Firestore!');
+      for (var entry in tree.entries) {
+        final id = entry.key;
+        final children = entry.value;
+        await contentRef.doc('ID$id').set({
+          'title': tasks[id],
+          'children': children,
+          'status': "do",
+          'description': "説明"
+        });
+        logger.info(contentRef.doc);
+      }
+
+      await userRef.set({
+        'tree': tree,
+        'tasks':tasks
+      }, SetOptions(merge: true));
+
+      logger.info(tree);
+      logger.info(tasks);
     }
-
 }
