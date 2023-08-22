eee8273facdb876fd4dedccb08379e9d2281da10
yuukinoki <usyuaki@gmail.com>
2023-08-22 14:55:07 +0900

    Update main.dart

--- dennys_web_app/lib/main.dart
+++ dennys_web_app/lib/main.dart
@@ -6,110 +6,107 @@ import 'firebase_options.dart';
 import 'package:dennys_web_app/global_setting/global_tree.dart';
 
 void main() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp();
-    runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-    final ManualResist dataService = ManualResist();
-    final Node sample = Node(
-        title: 'Sample Node Title',
-        children: ['Child1', 'Child2', 'Child3'],
-        status: 'InProgress',
-        description: 'This is a description for the sample node.',
+  final ManualResist dataService = ManualResist();
+  final Node sample = Node(
+    title: 'Sample Node Title',
+    children: ['Child1', 'Child2', 'Child3'],
+    status: 'InProgress',
+    description: 'This is a description for the sample node.',
+  );
+  final Map<String, List<int>> tree = {
+    '1': [2, 3, 4, 5, 6],
+    '2': [7, 8, 9],
+    '3': [10, 11, 12],
+    '4': [13, 14],
+    '5': [15, 16],
+    '6': [17, 18]
+  };
+
+  // Tasks data
+  final Map<String, String> tasks = {
+    '1': 'ゲームを作る',
+    '2': 'デザイン',
+    '3': 'プログラム',
+    '4': 'グラフィックス',
+    '5': 'サウンド',
+    '6': 'テスト',
+    '7': 'コンセプト',
+    '8': 'キャラ・ストーリー',
+    '9': 'ルール・メカニクス',
+    '10': 'エンジン選択',
+    '11': 'キャラ動き',
+    '12': 'ロジック・AI',
+    '13': 'キャラ・背景アート',
+    '14': 'アニメーション',
+    '15': 'BGM',
+    '16': '効果音',
+    '17': 'バグチェック',
+    '18': 'ユーザーテスト'
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Firestore Save Data')),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              GlobalTree.initialize(
+                  key: 'SomeUniqueKey', tree: tree, tasks: tasks);
+              var globalTree = GlobalTree.instance;
+              // For demonstration purposes, print the sample node details in the console
+              globalTree.printNodeList();
+              sample.display();
+            },
+            child: const Text('Save Data to Firestore'),
+          ),
+        ),
+      ),
     );
-    final Map<String, List<int>> tree = {
-        '1': [2, 3, 4, 5, 6],
-        '2': [7, 8, 9],
-        '3': [10, 11, 12],
-        '4': [13, 14],
-        '5': [15, 16],
-        '6': [17, 18]
-    };
-
-    // Tasks data
-    final Map<String, String> tasks = {
-        '1': 'ゲームを作る',
-        '2': 'デザイン',
-        '3': 'プログラム',
-        '4': 'グラフィックス',
-        '5': 'サウンド',
-        '6': 'テスト',
-        '7': 'コンセプト',
-        '8': 'キャラ・ストーリー',
-        '9': 'ルール・メカニクス',
-        '10': 'エンジン選択',
-        '11': 'キャラ動き',
-        '12': 'ロジック・AI',
-        '13': 'キャラ・背景アート',
-        '14': 'アニメーション',
-        '15': 'BGM',
-        '16': '効果音',
-        '17': 'バグチェック',
-        '18': 'ユーザーテスト'
-    };
-
-    @override
-    Widget build(BuildContext context) {
-        return MaterialApp(
-            home: Scaffold(
-                appBar: AppBar(title: const Text('Firestore Save Data')),
-                body: Center(
-                    child: ElevatedButton(
-                        onPressed: () {
-                            GlobalTree.initialize(
-                                key: 'SomeUniqueKey',
-                                tree: tree,
-                                tasks: tasks
-                            );
-                            var globalTree = GlobalTree.instance;
-                            // For demonstration purposes, print the sample node details in the console
-                            globalTree.printNodeList();
-                            sample.display();
-                        },
-                        child: const Text('Save Data to Firestore'),
-                    ),
-                ),
-            ),
-        );
-    }
+  }
 }
 
 class GameTreeInitializer {
-    // Tree data
-    final Map<String, List<int>> tree = {
-        '1': [2, 3, 4, 5, 6],
-        '2': [7, 8, 9],
-        '3': [10, 11, 12],
-        '4': [13, 14],
-        '5': [15, 16],
-        '6': [17, 18]
-    };
+  // Tree data
+  final Map<String, List<int>> tree = {
+    '1': [2, 3, 4, 5, 6],
+    '2': [7, 8, 9],
+    '3': [10, 11, 12],
+    '4': [13, 14],
+    '5': [15, 16],
+    '6': [17, 18]
+  };
 
-    // Tasks data
-    final Map<String, String> tasks = {
-        '1': 'ゲームを作る',
-        '2': 'デザイン',
-        '3': 'プログラム',
-        '4': 'グラフィックス',
-        '5': 'サウンド',
-        '6': 'テスト',
-        '7': 'コンセプト',
-        '8': 'キャラ・ストーリー',
-        '9': 'ルール・メカニクス',
-        '10': 'エンジン選択',
-        '11': 'キャラ動き',
-        '12': 'ロジック・AI',
-        '13': 'キャラ・背景アート',
-        '14': 'アニメーション',
-        '15': 'BGM',
-        '16': '効果音',
-        '17': 'バグチェック',
-        '18': 'ユーザーテスト'
-    };
+  // Tasks data
+  final Map<String, String> tasks = {
+    '1': 'ゲームを作る',
+    '2': 'デザイン',
+    '3': 'プログラム',
+    '4': 'グラフィックス',
+    '5': 'サウンド',
+    '6': 'テスト',
+    '7': 'コンセプト',
+    '8': 'キャラ・ストーリー',
+    '9': 'ルール・メカニクス',
+    '10': 'エンジン選択',
+    '11': 'キャラ動き',
+    '12': 'ロジック・AI',
+    '13': 'キャラ・背景アート',
+    '14': 'アニメーション',
+    '15': 'BGM',
+    '16': '効果音',
+    '17': 'バグチェック',
+    '18': 'ユーザーテスト'
+  };
 
-    /*Future<void> initialize() async {
+  /*Future<void> initialize() async {
         // In case there's some async tasks in the future
         GlobalTree globalTree = GlobalTree(
             key: 'SomeUniqueKey',
