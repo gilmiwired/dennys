4b7401b313844968c9275b0a4d6eab1a0d61ad0d
yuukinoki <usyuaki@gmail.com>
2023-08-22 19:36:35 +0900

    テスト用main

--- dennys_web_app/lib/main.dart
+++ dennys_web_app/lib/main.dart
@@ -1,120 +1,67 @@
-import 'package:dennys_web_app/Manual/Manual_Registration.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:dennys_web_app/logger/logger.dart';
-import 'firebase_options.dart';
 import 'package:dennys_web_app/global_setting/global_tree.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final ManualResist dataService = ManualResist();
-  final Node sample = Node(
-    title: 'Sample Node Title',
-    children: ['Child1', 'Child2', 'Child3'],
-    status: 'InProgress',
-    description: 'This is a description for the sample node.',
-  );
-  final Map<String, List<int>> tree = {
-    '1': [2, 3, 4, 5, 6],
-    '2': [7, 8, 9],
-    '3': [10, 11, 12],
-    '4': [13, 14],
-    '5': [15, 16],
-    '6': [17, 18]
-  };
-
-  // Tasks data
-  final Map<String, String> tasks = {
-    '1': 'ゲームを作る',
-    '2': 'デザイン',
-    '3': 'プログラム',
-    '4': 'グラフィックス',
-    '5': 'サウンド',
-    '6': 'テスト',
-    '7': 'コンセプト',
-    '8': 'キャラ・ストーリー',
-    '9': 'ルール・メカニクス',
-    '10': 'エンジン選択',
-    '11': 'キャラ動き',
-    '12': 'ロジック・AI',
-    '13': 'キャラ・背景アート',
-    '14': 'アニメーション',
-    '15': 'BGM',
-    '16': '効果音',
-    '17': 'バグチェック',
-    '18': 'ユーザーテスト'
-  };
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Firestore Save Data')),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              GlobalTree.initialize(
-                  key: 'SomeUniqueKey', tree: tree, tasks: tasks);
-              var globalTree = GlobalTree.instance;
-              // For demonstration purposes, print the sample node details in the console
-              globalTree.printNodeList();
-              sample.display();
-            },
-            child: const Text('Save Data to Firestore'),
-          ),
-        ),
+      title: 'Node Addition Test',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: MyHomePage(),
     );
   }
 }
 
-class GameTreeInitializer {
-  // Tree data
-  final Map<String, List<int>> tree = {
-    '1': [2, 3, 4, 5, 6],
-    '2': [7, 8, 9],
-    '3': [10, 11, 12],
-    '4': [13, 14],
-    '5': [15, 16],
-    '6': [17, 18]
-  };
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-  // Tasks data
-  final Map<String, String> tasks = {
-    '1': 'ゲームを作る',
-    '2': 'デザイン',
-    '3': 'プログラム',
-    '4': 'グラフィックス',
-    '5': 'サウンド',
-    '6': 'テスト',
-    '7': 'コンセプト',
-    '8': 'キャラ・ストーリー',
-    '9': 'ルール・メカニクス',
-    '10': 'エンジン選択',
-    '11': 'キャラ動き',
-    '12': 'ロジック・AI',
-    '13': 'キャラ・背景アート',
-    '14': 'アニメーション',
-    '15': 'BGM',
-    '16': '効果音',
-    '17': 'バグチェック',
-    '18': 'ユーザーテスト'
-  };
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
 
-  /*Future<void> initialize() async {
-        // In case there's some async tasks in the future
-        GlobalTree globalTree = GlobalTree(
-            key: 'SomeUniqueKey',
-            tree: tree,
-            tasks: tasks
-        );
+    // GlobalTreeの初期化
+    GlobalTree.initialize(
+      key: 'SomeUniqueKey',
+      tree: {
+        '1': [2, 3],
+        '2': [],
+        '3': [],
+      },
+      tasks: {
+        '1': 'Task 1',
+        '2': 'Task 2',
+        '3': 'Task 3',
+      },
+    );
+  }
 
-        // Print node list
-        globalTree.printNodeList();
-    }*/
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Node Addition Test'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // ノードを追加
+            GlobalTree.instance.addNode("New Task", "2", insertAschild: true);
+
+            // 結果をコンソールに表示
+            GlobalTree.instance.printNodeList();
+          },
+          child: Text('Add Node and Print Tree'),
+        ),
+      ),
+    );
+  }
 }
