034da1fe56d9bbf456565b89a51cb7199e786b1a
yuukinoki <usyuaki@gmail.com>
2023-08-30 17:00:51 +0900

    global_treeにおけるai応答からtree生成する機能

--- dennys_web_app/lib/global_setting/global_tree.dart
+++ dennys_web_app/lib/global_setting/global_tree.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:dart_openai/dart_openai.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:dennys_web_app/logger/logger.dart';
 
 class Node {
@@ -53,14 +56,37 @@ class GlobalTree {
         _tree = tree,
         _tasks = tasks;
 
-  static void initialize({
+// 通常のコンストラクタ
+  GlobalTree({
     required String key,
     required Map<String, List<int>> tree,
     required Map<String, String> tasks,
-  }) {
-    if (_singleton != null) return;
+  })  : _key = key,
+        _tree = tree,
+        _tasks = tasks {
+    // NodeListを初期化
+    for (var entry in tasks.entries) {
+      List<String> children =
+          tree[entry.key]?.map((e) => e.toString()).toList() ?? [];
+      _nodeList[entry.key] = Node(
+        title: entry.value,
+        parentID: entry.key,
+        children: children,
+        status: "do",
+        description: "説明",
+      );
+    }
+  }
 
-    _singleton = GlobalTree._internal(key: key, tree: tree, tasks: tasks);
+  static Future<GlobalTree> initializeAsync(String title) async {
+    //aiの応答からtree,tasksを生成してGlobalTreeを初期化するメソッド
+    if (_singleton != null) return _singleton!;
+
+    Map<String, dynamic> result = await generateTaskTree(title);
+    Map<String, List<int>> tree = result['tree'] as Map<String, List<int>>;
+    Map<String, String> tasks = result['tasks'] as Map<String, String>;
+
+    _singleton = GlobalTree._internal(key: title, tree: tree, tasks: tasks);
 
     // NodeListを初期化
     for (var entry in tasks.entries) {
@@ -74,6 +100,8 @@ class GlobalTree {
         description: "説明",
       );
     }
+
+    return _singleton!;
   }
 
   void collectAllChildNodes(String nodeKey) {
@@ -154,6 +182,84 @@ class GlobalTree {
         );
       }
     }
+  }
+
+  static Map<String, dynamic> extractTaskTree(String aiResponse) {
+    //応答をtree,tasksに整理
+    String validJsonString =
+        aiResponse.replaceAll("'", '"'); //正規表現によるシングルクォートをダブルクォートに変換
+    final RegExp pattern = RegExp(r"(\{.*\})");
+    final Match? match = pattern.firstMatch(validJsonString);
+
+    if (match != null) {
+      String extracted = match.group(1)!;
+      Map<String, dynamic> decoded = json.decode(extracted);
+
+      // treeをMap<String, List<int>>に変換
+      Map<String, List<int>> tree = {};
+      (decoded['tree'] as Map<String, dynamic>).forEach((key, value) {
+        tree[key] = (value as List).map((e) => int.parse(e)).toList();
+      });
+
+      // tasksをMap<String, String>に変換
+      Map<String, String> tasks = Map<String, String>.from(decoded['tasks']);
+
+      return {
+        'tree': tree,
+        'tasks': tasks,
+      };
+    } else {
+      print("Error: Failed to extract the task tree from the AI response.");
+      return {};
+    }
+  }
+
+  static Future<Map<String, dynamic>> generateTaskTree(String userGoal) async {
+    //userGoalに対するAIの応答を返す。（今のところは固定でtree,tasks生成のみ。そのうちフィードバックとかできるようにする。）
+    await dotenv.load(fileName: '.env');
+    OpenAI.apiKey = dotenv.get('OPEN_AI_API_KEY');
+
+    final messages = [
+      OpenAIChatCompletionChoiceMessageModel(
+        content:
+            "You are a task planner. The 'tree' represents the hierarchical structure of tasks where each key is a task ID and its corresponding value is a list of subtask IDs. The 'tasks' provides detailed descriptions for each task ID. Based on the user's goal, generate a task tree with both 'tree' and 'tasks'. If possible, generate at least 10 tasks, making sure that the tasks are MECE and that the level below the tree provides specific actions to guide the user toward the goal.The response should be structured as follows: {'tree': {'1': ['2', '3',...], '2': ['4', '5',...], '3': [],...}, 'tasks': {'1': 'Goal description', '2': 'Subtask 1 description',...}}. Ensure that the information is provided in a single line without any line breaks.Response in japanese. Exclude all other explanations.",
+        role: OpenAIChatMessageRole.system,
+      ),
+      OpenAIChatCompletionChoiceMessageModel(
+        content: userGoal,
+        role: OpenAIChatMessageRole.user,
+      ),
+    ];
+
+    int retryCount = 0;
+    const int maxRetries = 5;
+    Map<String, dynamic> result = {};
+
+    while (retryCount < maxRetries) {
+      try {
+        final response = await OpenAI.instance.chat.create(
+          model: 'gpt-3.5-turbo',
+          messages: messages,
+        );
+
+        final aiResponse = response.choices.first.message.content;
+        result = extractTaskTree(aiResponse);
+
+        if (result.containsKey('tree') && result.containsKey('tasks')) {
+          break;
+        }
+      } catch (error) {
+        print("Error occurred: $error");
+      }
+
+      retryCount++;
+    }
+
+    if (retryCount == maxRetries) {
+      print("Maximum retries reached without a valid response.");
+    }
+
+    return result;
   }
 
   // シングルトンのインスタンスをリセットするメソッド
