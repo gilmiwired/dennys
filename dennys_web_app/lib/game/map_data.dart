599b06e85860eb3ce52a66e297139cef6ff8d7d3
gilmi <65973449+gilmiwired@users.noreply.github.com>
2023-09-09 23:10:11 +0900

    ほぼかんせ

--- dennys_web_app/lib/game/map_data.dart
+++ dennys_web_app/lib/game/map_data.dart
@@ -6,7 +6,7 @@ import 'package:flame/src/extensions/vector2.dart';
 
 //wall
 final Map<int, Vector2> spriteCoordinates = {
-  -1 :Vector2(5, 2),
+  -1 :Vector2(0, 0),
   0: Vector2(0, 0),  // none_tile
   1: Vector2(5, 2),  // TopWall
   2: Vector2(4, 2),  // TopEdge
@@ -31,10 +31,21 @@ final Map<int, Vector2> spriteCoordinates = {
   13: Vector2(5, 2), // DownEdge
   14: Vector2(4, 2), // DownWall
   15: Vector2(0, 0), // DownWall
+
+  16: Vector2(6, 1),
+  17: Vector2(7, 1),
+  18: Vector2(1, 1),
+  19: Vector2(2, 1),
+
+  20: Vector2(2, 4),
+  21: Vector2(1, 4),
+  22: Vector2(6, 4),
+  23: Vector2(7, 4),
+  24: Vector2(0,0),
 };
 
 final Map<int, Vector2> spriteCoordinatesFloor = {
-  -1 :Vector2(14, 2),
+  -1 :Vector2(15, 2),
   0: Vector2(0, 0),  // none_tile
 
   1: Vector2(15, 2),  // TopWall
@@ -60,6 +71,18 @@ final Map<int, Vector2> spriteCoordinatesFloor = {
   13: Vector2(0, 0), // DownEdge
   14: Vector2(16, 5), // DownWall
   15: Vector2(15, 2), // DownWall
+
+  16: Vector2(15, 2),
+  17: Vector2(15, 2),
+  18: Vector2(17, 5),//
+
+  19: Vector2(15, 4),
+  20: Vector2(15, 1),
+  21: Vector2(15, 1),
+  22: Vector2(15, 2),//defwall
+  23: Vector2(15,2),
+  24: Vector2(15,2),
+
 };
 
 final List<List<int>> floorData = [
