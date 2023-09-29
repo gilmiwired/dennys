565ce98cbe920797b1e2d4521a62a2fed5f4353e
gilmi <65973449+gilmiwired@users.noreply.github.com>
2023-09-29 17:20:37 +0900

    Enemy追加、タイトル位置調整
    gameの内容はほぼこれで完成

--- dennys_web_app/lib/main.dart
+++ dennys_web_app/lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:dennys_web_app/login/login_page.dart';
 import 'package:dennys_web_app/register/registration_page.dart';
+import 'package:dennys_web_app/global_setting/global_tree.dart';
 import 'package:dennys_web_app/profile/user_data.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:dennys_web_app/Screen/Home.dart';
 
 import 'package:dennys_web_app/game/test_game.dart';
 
@@ -38,6 +40,32 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     // Initialization code here
   }
+
+  // Step 1: Define the controller for the TextField
+  final TextEditingController _textController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textController.dispose();  // Don't forget to dispose the controller when not in use
+    super.dispose();
+  }
+
+  Future<void> _onButtonPressed() async {
+    // Retrieve the value from the TextField
+    String inputValue = _textController.text;
+    await GlobalTree.initialize(title: "My Task Tree", isAsync: true);
+    GlobalTree myTree = GlobalTree.instance;
+    Node? rootNode = myTree.nodeList["1"];
+
+    if (rootNode != null) {
+      myTree.assign_Coordinates(rootNode);
+    }
+    myTree.displayAllNodes();
+    myTree.printNodeList();
+    print("Input Value: $inputValue");
+    // You can call any other specific function here using the inputValue
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -76,6 +104,70 @@ class _HomePageState extends State<HomePage> {
               },
               child: const Text('Game'),
             ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Initialize the GlobalTree
+                GlobalTree.initialize(
+                  title: "My Task Tree",
+                  tree: {
+                    '1': [2, 3, 4, 5, 6],
+                    '2': [7, 8, 9],
+                    '3': [10, 11, 12],
+                    '4': [13, 14],
+                    '5': [15, 16]
+                  },
+                  tasks: {
+                    '1': 'ゲームを作る',
+                    '2': 'デザイン',
+                    '3': 'プログラム',
+                    '4': 'グラフィックス',
+                    '5': 'サウンド',
+                    '6': 'テスト',
+                    '7': 'コンセプト',
+                    '8': 'キャラ・ストーリー',
+                    '9': 'ルール・メカニクス',
+                    '10': 'エンジン選択',
+                    '11': 'キャラ動き',
+                    '12': 'ロジック・AI',
+                    '13': 'キャラ・背景アート',
+                    '14': 'アニメーション',
+                    '15': 'BGM',
+                    '16': '効果音'
+                  },
+                );
+
+                GlobalTree myTree = GlobalTree.instance;
+
+                Node? rootNode = myTree.nodeList["1"];
+
+                if (rootNode != null) {
+                  myTree.assign_Coordinates(rootNode);
+                }
+                myTree.displayAllNodes();
+              },
+              child: const Text('Game'),
+            ),
+
+
+            // Step 2: Add the TextField to the Column
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                controller: _textController,
+                decoration: InputDecoration(
+                  hintText: 'Enter some text here',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+
+            // Step 3: Add the button
+            ElevatedButton(
+              onPressed: _onButtonPressed,
+              child: const Text('Submit Text'),
+            ),
+
           ],
         ),
       ),
