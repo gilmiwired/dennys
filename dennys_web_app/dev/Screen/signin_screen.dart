7d916ea3413fb36c91204146e991cc4e75f6751e
yuukinoki <usyuaki@gmail.com>
2024-02-14 14:45:31 +0900

    push

---
+++ dennys_web_app/dev/Screen/signin_screen.dart
@@ -0,0 +1,179 @@
+// signin_screen.dart
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'signup_screen.dart';
+import 'Welcome.dart';
+
+class SignInScreen extends StatefulWidget {
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  Future<void> _signIn() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      try {
+        UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+            email: _emailController.text, password: _passwordController.text);
+        print("Sign in successful for email: ${_emailController.text}");
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Successfully Signed In!')),
+        );
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => WelcomeScreen(user: userCredential.user)));
+      } catch (e) {
+        print("Sign in failed with error: $e");
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Sign In Failed: $e')),
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Background gradient
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment(0.00, -1.00),
+                end: Alignment(0, 1),
+                colors: [Color(0xFF102425), Color(0xFF723873)],
+              ),
+            ),
+          ),
+          // Content
+          Column(
+            children: [
+              // Header
+              _buildCustomHeader(),
+              // Sign In Form
+              Expanded(
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          _buildInputField('Email', 'Enter your email', _emailController),
+                          SizedBox(height: 20),
+                          _buildInputField('Password', 'Enter your password', _passwordController),
+                          SizedBox(height: 40),
+                          _buildSignInButton(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCustomHeader() {
+    return Container(
+      width: double.infinity,
+      height: 98,
+      color: Color(0xFFF2C9E7),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Randuraft',
+              style: TextStyle(
+                color: Color(0xFFBE60AE),
+                fontSize: 32,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+            GestureDetector(  // GestureDetectorを追加
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SignUpScreen()),  // SignInScreenに遷移
+                );
+              },
+              child: const Text(
+                'Sign up',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 28,
+                  fontFamily: 'Noto Sans Japanese',
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildInputField(String label, String hint, TextEditingController controller, {bool isPassword = true}) {
+    return TextField(
+      controller: controller,
+      obscureText: isPassword,  // パスワードフィールドの場合、テキストを隠します
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(
+          color: Color(0xFFD0C7C7),
+          fontSize: 20,
+          fontFamily: 'Inter',
+          fontWeight: FontWeight.w600,
+        ),
+        filled: true,
+        fillColor: Color(0xFF723873),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(width: 2, color: Color(0xFFBE60AE)),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(width: 2, color: Color(0xFFBE60AE)),
+        ),
+      ),
+    );
+  }
+
+
+  Widget _buildSignInButton() {
+    return ElevatedButton(
+      onPressed: _signIn,  // ここで_signUpメソッドを呼び出します
+      style: ElevatedButton.styleFrom(
+        primary: Color(0xFF102425),
+        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(50),
+        ),
+      ),
+      child: Text(
+        'Sign in',
+        style: TextStyle(
+          color: Color(0xFFF2C9E7),
+          fontSize: 20,
+          fontFamily: 'Roboto',
+          fontWeight: FontWeight.w900,
+        ),
+      ),
+    );
+  }
+// _buildCustomHeader(), _buildInputField(), _buildSignInButton()
+}
--- dennys_web_app/dev/view/custom_painter.dart
+++ dennys_web_app/dev/view/custom_painter.dart
@@ -3,11 +3,10 @@ import 'package:dennys_web_app/global_setting/global_tree.dart';
 
 class TreePainter extends CustomPainter {
   final Map<String, Node> nodeList;
-  final double offsetX; // 追加: X方向のオフセット
-  final double offsetY; // 追加: Y方向のオフセット
+  final double offsetX;  // 追加: X方向のオフセット
+  final double offsetY;  // 追加: Y方向のオフセット
 
-  TreePainter(this.nodeList,
-      {this.offsetX = 100, this.offsetY = 100}); // オフセットを引数で受け取る
+  TreePainter(this.nodeList, {this.offsetX = 100, this.offsetY = 100});  // オフセットを引数で受け取る
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -26,10 +25,8 @@ class TreePainter extends CustomPainter {
       final int nodeHeight = 50;
 
       final rect = Rect.fromPoints(
-        Offset(node.x.toDouble() + offsetX,
-            node.y.toDouble() + offsetY), // オフセットを加算
-        Offset(node.x.toDouble() + nodeWidth + offsetX,
-            node.y.toDouble() + nodeHeight + offsetY), // オフセットを加算
+        Offset(node.x.toDouble() + offsetX, node.y.toDouble() + offsetY),  // オフセットを加算
+        Offset(node.x.toDouble() + nodeWidth + offsetX, node.y.toDouble() + nodeHeight + offsetY),  // オフセットを加算
       );
 
       canvas.drawRect(rect, paint);
@@ -41,25 +38,15 @@ class TreePainter extends CustomPainter {
       );
       textPainter.layout();
       textPainter.paint(
-          canvas,
-          Offset(node.x.toDouble() + 5 + offsetX,
-              node.y.toDouble() + 5 + offsetY)); // オフセットを加算
+          canvas, Offset(node.x.toDouble() + 5 + offsetX, node.y.toDouble() + 5 + offsetY));  // オフセットを加算
 
       // エッジを描画
       for (String childID in node.children) {
         Node? child = nodeList[childID];
         if (child != null) {
           canvas.drawLine(
-            Offset(
-                node.x.toDouble() + offsetX,
-                node.y.toDouble() +
-                    nodeHeight / 2 +
-                    offsetY), // x座標を親ノードの左端に, y座標を中央に
-            Offset(
-                child.x.toDouble() + nodeWidth + offsetX,
-                child.y.toDouble() +
-                    nodeHeight / 2 +
-                    offsetY), // x座標を子ノードの右端に, y座標を中央に
+            Offset(node.x.toDouble() + offsetX, node.y.toDouble() + nodeHeight / 2 + offsetY),  // x座標を親ノードの左端に, y座標を中央に
+            Offset(child.x.toDouble() + nodeWidth + offsetX, child.y.toDouble() + nodeHeight / 2 + offsetY),  // x座標を子ノードの右端に, y座標を中央に
             Paint()
               ..color = Colors.red
               ..strokeWidth = 2,
@@ -75,6 +62,7 @@ class TreePainter extends CustomPainter {
   }
 }
 
+
 class TreePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -82,12 +70,13 @@ class TreePage extends StatelessWidget {
     GlobalTree.instance.calculateLayout();
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Tree Structure'),
-        ),
-        body: CustomPaint(
-          painter: TreePainter(GlobalTree.instance.nodeList,
-              offsetX: 500, offsetY: 200),
-        ));
+      appBar: AppBar(
+        title: Text('Tree Structure'),
+      ),
+      body: CustomPaint(
+        painter: TreePainter(GlobalTree.instance.nodeList, offsetX: 500, offsetY: 200),
+      )
+    );
   }
 }
+
