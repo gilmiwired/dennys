6b10b1d51859b59c7ac22368816af528c16e2519
gilmi <65973449+gilmiwired@users.noreply.github.com>
2024-05-07 07:30:13 +0900

    Update task.py

--- randuraft_endpoint/api/routers/task.py
+++ randuraft_endpoint/api/routers/task.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter
 
 from api.models.task import ChatRequest, ChatResponse
-from randuraft_endpoint.services.create_chat import get_chat_completion
+from services.create_chat import get_chat_completion
 
 router = APIRouter()
 
 
 @router.post("/chat", response_model=ChatResponse)
 def chat(request: ChatRequest):
-    response_message = get_chat_completion(request.dict())
+    response_message = get_chat_completion(request.message)
     return ChatResponse(response_message=response_message)
 
 
