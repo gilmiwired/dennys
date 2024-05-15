9b3129057f638dc46afddc4dc0653731f01d8c65
gilmi <65973449+gilmiwired@users.noreply.github.com>
2024-05-15 16:03:07 +0900

    feat: CORS設定

--- randuraft_endpoint/api/app.py
+++ randuraft_endpoint/api/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.routers.task import router as task
 
@@ -6,3 +7,17 @@ from api.routers.task import router as task
 # TODO: Authいれる
 app = FastAPI()
 app.include_router(task)
+origins = [
+    "http://localhost:3000",
+    "http://localhost:61435",  # FlutterのWebビルドが使用するポート
+    "http://localhost",
+]
+
+# CORSの設定
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
