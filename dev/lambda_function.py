63dbfc4026f30ce8c2cfcafbfaf6c80002ee09ad
gilmi <65973449+gilmiwired@users.noreply.github.com>
2024-05-21 21:17:45 +0900

    refac:

--- dev/lambda_function.py
+++ dev/lambda_function.py
@@ -6,22 +6,25 @@ import requests
 
 
 def transform_message(original_message):
-    # openai_api_key　= chatGPTのAPI
-    openai_api_key = ""
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+    if not openai_api_key:
+        raise ValueError("OPENAI_API_KEY is not set")
+
     openai.api_key = openai_api_key
 
-    prompt_for_summarization = f"{original_message}これを日本語で要約して "
+    prompt_for_summarization = f"{original_message} これを日本語で要約して"
 
-    # GPT-3.5-turboの応答取得
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": prompt_for_summarization}],
-    )
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": prompt_for_summarization}],
+        )
+        return response.choices[0].message["content"]
+    except openai.error.OpenAIError as e:
+        print(f"OpenAI API error: {e}")
+        return "Error in generating summary"
 
-    return response.choices[0].message["content"]
 
-
-# こっから下はslackとawsの通信関連
 def lambda_handler(event, context):
     body = json.loads(event["body"])
     challenge_value = body.get("challenge")
@@ -32,14 +35,23 @@ def lambda_handler(event, context):
     original_message = body["event"]["text"]
     transformed_message = transform_message(original_message)
 
-    # webhook用のurl
-    webhook_url = ""
+    webhook_url = os.getenv("WEBHOOK_URL")
+    if not webhook_url:
+        raise ValueError("WEBHOOK_URL is not set")
 
-    response = requests.post(
-        webhook_url,
-        headers={"Content-Type": "application/json"},
-        data=json.dumps({"text": transformed_message}),
-    )
+    try:
+        response = requests.post(
+            webhook_url,
+            headers={"Content-Type": "application/json"},
+            data=json.dumps({"text": transformed_message}),
+        )
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Error sending message to webhook: {e}")
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"message": "Error in sending message to webhook"}),
+        }
 
     return {
         "statusCode": 200,
